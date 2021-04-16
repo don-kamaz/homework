f2690e66029349e437a88338cedd190445d427f1
Mikhail Belovolov <don.belovolov2018@yandex.ru>
2021-04-16 16:01:48 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector('.btn');
 let restMain = document.querySelector('rest p'}
 
-btn.onclick = function () {
+btn.onclick = function (evt) {
+  evt.priventDefault();
   restMain.style.marginLeft = '50px';
 }
