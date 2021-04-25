8cb8f0c95c5cdde911310effafe767cee4b6a391
Mikhail Belovolov <don.belovolov2018@yandex.ru>
2021-04-25 22:38:09 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -17,7 +17,17 @@ $(function() {
     });
   });
   $(window).scroll(function() {
-    $('.rest-box  .tree').each(function(){
+    $('.mail .section-title').each(function(){
+        var imagePos = $(this).offset().top;
+  
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) {
+            $(this).addClass("fadeInLeft");
+        }
+    });
+  });
+  $(window).scroll(function() {
+    $('.mail .btn-form').each(function(){
         var imagePos = $(this).offset().top;
   
         var topOfWindow = $(window).scrollTop();
