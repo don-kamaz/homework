8a6bb1cf71fd2f2ec114a143ec6442f7c6d1f41a
Mikhail Belovolov <don.belovolov2018@yandex.ru>
2021-04-29 17:16:45 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(function() {
   
         var topOfWindow = $(window).scrollTop();
         if (imagePos < topOfWindow+650) {
-            $(this).addClass("fadeInUp");
+            $(this).addClass("fadeInRight");
         }
     });
   });
