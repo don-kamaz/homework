efb0945c129ee45291487dbdcd4a614607f950f9
Mikhail Belovolov <don.belovolov2018@yandex.ru>
2021-04-29 17:20:49 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -26,14 +26,4 @@ $(function() {
         }
     });
   });
-  $(window).scroll(function() {
-    $('.mail .btn-form').each(function(){
-        var imagePos = $(this).offset().top;
-  
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow+650) {
-            $(this).addClass("fadeInRight");
-        }
-    });
-  });
 })
